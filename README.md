e10fc41b33f360f572252fda3a003c472c6317dd
anoop rajoriya <anooprajoriya9827@gmail.com>
2026-05-05 08:46:09 +0530

    add: initila setup of auth app

---
+++ README.md
@@ -0,0 +1,1 @@
+# Chaicode Free Api Showcase
---
+++ vanilla-js-auth-app/src/main.js
@@ -0,0 +1,10 @@
+const KEY = "free-api-auth-data";
+const tokens = JSON.parse(localStorage.getItem(KEY));
+
+class Service {}
+
+const toast = {
+  success: () => {},
+  alert: () => {},
+  clear: () => {},
+};
